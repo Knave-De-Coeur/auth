a016b1615b59cdaf4e8119adf7b5d15f5d948e4c
Knave-De-Coeur <alexanderm.1496@gmail.com>
2023-03-23 10:16:16 +0100

    Fixed all bugs in service, now running as api and as nats service.

--- app/classes/auth_service.py
+++ app/classes/auth_service.py
@@ -1,52 +1,35 @@
 import asyncio
+from typing import Any
+
 from fastapi import FastAPI
 from nats.aio.client import Client as Nats, Msg as Msg, Subscription as Sub, Awaitable as Awaitable, Type as Type
-import signal
+
 from app.routers import users, home
-from typing import Any
-import json
 
 
 class AuthService(FastAPI):
     nc = Nats | None
     sub = Sub | None
     shutdown = bool
-    # loop = asyncio.get_event_loop()
 
     def __init__(self, **extra: Any):
         super().__init__(**extra)
         self.nc = Nats()
         self.sub = None
-        self.shutdown = False
-
-        # Handles graceful shutdown
-        # signal.signal(signal.SIGINT, self.exit_graceful)
-        # signal.signal(signal.SIGTERM, self.exit_graceful)
-
-    # def exit_graceful(self, signum, frame):
-    #     print('Received:', signum, ": ", frame)
-    #     self.shutdown = True
 
     async def connect_to_nats(self):
         await self.nc.connect("nats://127.0.0.1:4222")
 
     async def set_up_subscriptions(self):
-        async def response_handle(msg):
-            m = json.loads(msg.data.decode())
-            # self.nc.msgs.append(message)
-            return await self.nc.publish(m.reply, b'secretpassword')
-
-        self.sub = await self.nc.subscribe("auth.generate.password", cb=response_handle)
+        try:
+            self.sub = await self.nc.subscribe("auth.generate.password", cb=self.process_request)
+        except Exception as e:
+            print(e)
+            return
 
     async def process_request(self, m=Type[Msg]) -> Awaitable[None] | None:
         print(f"Received a message on '{m.subject} {m.reply}': {m.data.decode()}")
         return await self.nc.publish(m.reply, b'secretpassword')
-
-    async def run(self):
-        print("running App, awaiting messages: ")
-
-        while True:
-            await asyncio.sleep(1)
 
     def setup_api(self):
         self.include_router(home.home_router)
--- app/main.py
+++ app/main.py
@@ -7,20 +7,29 @@ service = auth.AuthService()
 service.setup_api()
 
 
-async def app():
+async def create_webserver(port):
+    server_config = uvicorn.Config(service, port=port, log_level="info")
+    server = uvicorn.Server(server_config)
+    await server.serve()
 
-    task1 = asyncio.create_task(service.connect_to_nats())
-    await task1
 
-    task2 = asyncio.create_task(service.set_up_subscriptions())
-    await task2
+async def main():
+    task1 = asyncio.create_task(create_webserver(8000))
+    task2 = service.connect_to_nats()
+    task3 = service.set_up_subscriptions()
+    _ = await asyncio.gather(
+            task1,
+            task2,
+            task3,
+    )
+
+    # terminates nats connection and flushes subscriptions
+    await service.stop()
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(app())
-        # TODO fix subscription response
-        uvicorn.run(service, host="127.0.0.1", port=8000, log_level="info")
+        asyncio.run(main())
     except Exception as e:
         print(e)
         sys.exit(0)
